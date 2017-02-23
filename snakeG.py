b3d4a0b99665dadada1859e8b22c05f83fb58054
tkohlhauff <tkohlhauff@gmail.com>
2017-02-23 12:50:41 -0600

    Add files via upload

--- snakeG.py
+++ snakeG.py
@@ -53,9 +53,12 @@ def main():
         
 def chkMove(move):
     global currY,currX,collected,rect,collect,fps
+    #draws the snake
     for x in last:
         rect=pygame.draw.rect(screen,WHITE,(x[0],x[1],HEIGHT,HEIGHT),0)
+    #draws the block needed to be picked up
     ranBlock=pygame.draw.rect(screen,WHITE,(ranx,rany,HEIGHT,HEIGHT),0)
+    #sets movement determined by last input
     if move=='up':
         currY-=HEIGHT
     elif move=='down':
@@ -64,24 +67,33 @@ def chkMove(move):
         currX-=HEIGHT
     elif move=='right':
         currX+=HEIGHT
+    #create list for last position
     lastXY=[]
     lastXY.append(currX)
     lastXY.append(currY)
+    #reverse list to set last movement to first position
     lastXY.reverse()
     lastXY.reverse()
+    #append last list with both positions
     last.append(lastXY)
+    #delete extra movements from last
     if len(last)>collected:
         del last[0]
+    #check collision with block to pick up
     if rect.colliderect(ranBlock):
         collect=False
         collected+=1
+        #speed up game every 3 pick ups
         if collected%3==0:
             fps+=3
+    #check wall collisions
     if -15>currX or currX>1015 or -15>currY or currY>1015:
         pygame.quit()
+    #check collision with self
     for x in last[:-1]:
         if x==lastXY:
             pygame.quit()
+#create position for random block
 def ranBlock():
     global ranx, rany, collect
     collect=True
