b6fd4ab73f5ecd74b4c6bee168998c3546b4070d
tkohlhauff <tkohlhauff@gmail.com>
2017-03-21 23:55:03 -0500

    Add files via upload

--- Starship.py
+++ Starship.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -30,14 +31,18 @@ y_change = 0
 
 lastDir = 3 # 0 = left, 1 = up, 2 = right, 3 = down
 # Sprite animation step count once it reaches three set topCount to true and go down
-topCount = False
+stepCount = 0
 an = 0
 treantIdle=0
 treantCount=0
 playerImg = pygame.image.load('Wizard-front-2.png')
+pygame.key.set_repeat(33, 33)
 
 pressed=False
-
+left=False
+right=False
+up=False
+down=False
 # Arrays containing sprite animations; 0 is up, 1 is Down, 2 is left, 3 is right
 sprites=[['Wizard-back-1.png','Wizard-back-2.png','Wizard-back-3.png'],['Wizard-front-1.png','Wizard-front-2.png','Wizard-front-3.png'],
          ['Wizard-left-1.png','Wizard-left-2.png','Wizard-left-3.png'],['Wizard-right-1.png','Wizard-right-2.png','Wizard-right-3.png']]
@@ -50,17 +55,18 @@ def player(img,x,y):
     gameDisplay.blit(playerImg,(x,y))
 
 def movement():
-    global lastDir, topCount, an,treantIdle,treantCount
+    global lastDir, stepCount, an,treantIdle,treantCount
     global x, y, x_change, y_change, sprites,pressed
-    #Checks direction from keypressed and moves count for sprite animation
-    if topCount:
-        an -= 1
-    else:
-        an += 1
-    if an==2:
-        topCount=True
-    elif an==0:
-        topCount=False
+    global right,left,up,down
+    #Count for walk animation
+    stepCount+=1
+    if stepCount==3:
+        an=0
+    if stepCount==9:
+        an=1
+    if stepCount==12:
+        an=2
+        stepCount=0
     # Movement for the player
     if x + x_change >= 0 and x + x_change <= display_width - player_width:
             x += x_change
@@ -68,58 +74,31 @@ def movement():
         y += y_change
 
     #check for key change during animation as well as let go of key
-    for event in pygame.event.get():
-        if event.type==pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                pressed=True
-                lastDir=0
-            if event.key == pygame.K_UP:
-                pressed=True
-                lastDir=1
-            if event.key == pygame.K_RIGHT:
-                pressed=True
-                lastDir=2
-            if event.key == pygame.K_DOWN:
-                pressed=True
-                lastDir = 3
-        if event.type==pygame.KEYUP:
-            pressed=False
+        
     treantIdle+=1
-    if treantIdle%33==0:
+    if treantIdle==30:
         treantCount=1
-    if treantIdle%70==0:
+    if treantIdle==60:
         treantCount=0
+        treantIdle=0
     draw()
-##    #Draws the sprites based on direction and change in x,y position      
-##    gameDisplay.fill(white)
-##    if lastDir == 0:
-##        player(sprites[2][an],x,y)
-##        
-##    elif lastDir == 1:
-##        player(sprites[0][an],x,y)
-##        
-##    elif lastDir == 2:
-##        player(sprites[3][an],x,y)
-##
-##    elif lastDir == 3:
-##        player(sprites[1][an],x,y)
-##    
-##    pygame.display.update()
-##    clock.tick(30)
 def draw():
-    #Draws the sprites based on direction and change in x,y position      
-    gameDisplay.fill(white)
+        
+    #background
+    back = pygame.image.load('Sand-5.png').convert()
+    for i in range(math.floor(display_width / 32) + 1):
+        for z in range(math.floor(display_height / 32) + 1):
+            gameDisplay.blit(back,(i*32,z*32))
+    #Draws the sprites based on direction and change in x,y position  
     treant(treantSprite[treantCount])
-    if lastDir == 0:
-        player(sprites[2][an],x,y)
-        
-    elif lastDir == 1:
-        player(sprites[0][an],x,y)
-        
-    elif lastDir == 2:
-        player(sprites[3][an],x,y)
 
-    elif lastDir == 3:
+    if lastDir==0:
+        player(sprites[2][an],x,y)
+    if lastDir==1:
+        player(sprites[0][an],x,y)
+    if lastDir==2:
+        player(sprites[3][an],x,y)
+    if lastDir==3:
         player(sprites[1][an],x,y)
     
     pygame.display.update()
@@ -127,6 +106,7 @@ def draw():
     
 def game_loop():
     global lastDir, x_change, y_change, pressed,treantIdle,treantCount
+    global right,left,up,down
 
     # Game close boolean
     gameExit = False
@@ -141,42 +121,53 @@ def game_loop():
                 if event.key == pygame.K_ESCAPE:
                     gameExit = True
                 if event.key == pygame.K_LEFT:
-                    pressed=True
-                    lastDir=0
-                if event.key == pygame.K_UP:
-                    pressed=True
-                    lastDir=1
-                if event.key == pygame.K_RIGHT:
-                    pressed=True
-                    lastDir=2
-                if event.key == pygame.K_DOWN:
-                    pressed=True
-                    lastDir = 3
-        #use a loop for button pressses, needed for long presses
-            while pressed==True:
-                if lastDir == 0:
                     x_change = -5
                     movement()
                     x_change=0
-                if lastDir == 1:
+                    lastDir=0
+                if event.key == pygame.K_UP:
                     y_change = -5
                     movement()
-                    y_change=0 
-
-                if lastDir == 2:
+                    y_change=0
+                    lastDir=1
+                if event.key == pygame.K_RIGHT:
                     x_change = 5
                     movement()
-                    x_change=0 
-
-                if lastDir == 3:
+                    x_change=0
+                    lastDir=2
+                if event.key == pygame.K_DOWN:
                     y_change = 5
                     movement()
                     y_change=0
+                    lastDir=3
+        #use a loop for button pressses, needed for long presses
+##            while pressed==True:
+##                if left:
+##                    x_change = -5
+##                    movement()
+##                    x_change=0
+##                    lastDir=0
+##                elif up:
+##                    y_change = -5
+##                    movement()
+##                    y_change=0 
+##                    lastDir=1
+##                elif right:
+##                    x_change = 5
+##                    movement()
+##                    x_change=0 
+##                    lastDir=2
+##                elif down:
+##                    y_change = 5
+##                    movement()
+##                    y_change=0
+##                    lastDir=3
         treantIdle+=1
-        if treantIdle%33==0:
+        if treantIdle==30:
             treantCount=1
-        if treantIdle%70==0:
+        if treantIdle==60:
             treantCount=0
+            treantIdle=0
         draw()
     
    
